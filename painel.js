5605aaa20603b0b51e0d26b9a61c632a1c19a684
BernardoFilho10 <bernardodev2022@gmail.com>
2022-05-16 22:47:48 -0300

    arrumando o codigo

--- painel.js
+++
@@ -1,8 +0,0 @@
- function sortear() {
-    let participantes = 100;
-
-    let sorteio = Math.round(Math.random() * participantes);
-
-    document.getElementById("resultado").innerHTML = sorteio;
-  
-}
